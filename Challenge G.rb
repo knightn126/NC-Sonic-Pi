8ea7759da870a2ff49c4a569aa785a5e5f40266e
knightn126 <nate.castillo@me.com>
2026-03-10 11:09:09 -0700

    made more universal

--- Challenge G.rb
+++ Challenge G.rb
@@ -3,21 +3,30 @@ use_synth :saw
 
 notes = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
 
+
 define :play_sequence do |notes, time = 0.5|
-  notes.each do |note|
-    play note
+  i = 0                          
+  while i < notes.length do      
+    play notes[i]                
     sleep time
+    i = i + 1                    
   end
 end
 
+loop_count = 0                   
+
 live_loop :main_theme do
   with_fx :distortion do
-    stop if tick == 4
+    if loop_count >= 4            
+      stop
+    end
     play_sequence notes
+    loop_count = loop_count + 1
   end
 end
 
 sleep 16
+
 
 use_bpm 120
 use_synth :fm
@@ -26,10 +35,13 @@ use_synth_defaults sustain: 0.25
 sound  = [:e3, :e3, :g3, :e3, :r,  :d3, :c3, :b2]
 timing = [1.5, 0.5, 0.75, 0.25, 0.5, 0.5, 2.0, 2.0]
 
+
 define :play_timed_sequence do |notes, timings|
-  notes.zip(timings).each do |note, duration|
-    play note
-    sleep duration
+  i = 0
+  while i < notes.length do     
+    play notes[i]               
+    sleep timings[i]            
+    i = i + 1
   end
 end
 
